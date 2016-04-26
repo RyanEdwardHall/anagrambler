aa9323eadc6f2046f22604cbf569007b17c08098
Ryan Hall <ryan.hall@vidyard.com>
2016-04-25 22:16:40 -0400

    tabs to spaces

--- Trie.js
+++ Trie.js
@@ -8,65 +8,65 @@ class Trie {
     this.insert(words);
   }
   insert(words) {
-  	words.forEach(word => {
-  		let englishWord = word;
-  		word = word.split('').sort().join('');
-  		var characters = word.split('');
-  		var path = this.data;
-		  characters.forEach((character, index) => {
-			  if (!path.hasOwnProperty(character)) {
-				  path[character] = {};
-			  }
-			  if (index === (word.length - 1)) {
-				  if (!path[character].hasOwnProperty('_words')) {
-					  path[character]._words = [];
-				  }
-				  path[character]._words.push(englishWord);
-			  } else {
-				  path = path[character];
-			  }
-		  });
-  	});
+    words.forEach(word => {
+      let englishWord = word;
+      word = word.split('').sort().join('');
+      var characters = word.split('');
+      var path = this.data;
+      characters.forEach((character, index) => {
+        if (!path.hasOwnProperty(character)) {
+          path[character] = {};
+        }
+        if (index === (word.length - 1)) {
+          if (!path[character].hasOwnProperty('_words')) {
+            path[character]._words = [];
+          }
+          path[character]._words.push(englishWord);
+        } else {
+          path = path[character];
+        }
+      });
+    });
   }
   search(letters) {
-  	var noDupes = {};
-  	var self = this;
-  	letters = letters.split('').sort();
-  	function searcher(prefix, remainder) {
-  		var path = self.data;
-		if (noDupes[prefix] === undefined) {
-			noDupes[prefix] = null;
-  		}
-  		try {
-			prefix.split('').forEach(character => {
-				path = path[character];
-			});
-	  		if (path._words !== undefined) {
-	  			noDupes[prefix] = path._words;
-	  		}
-  		} catch (e) {
-  			path = null;
-  		}
+    var noDupes = {};
+    var self = this;
+    letters = letters.split('').sort();
+    function searcher(prefix, remainder) {
+      var path = self.data;
+    if (noDupes[prefix] === undefined) {
+      noDupes[prefix] = null;
+      }
+      try {
+      prefix.split('').forEach(character => {
+        path = path[character];
+      });
+        if (path._words !== undefined) {
+          noDupes[prefix] = path._words;
+        }
+      } catch (e) {
+        path = null;
+      }
 
-  		remainder.forEach((letter, index) => {
-  			var remainderCopy = remainder.slice();
-  			var popRemainder = remainderCopy.splice(index, 1);
-  			var newPrefix = (prefix + popRemainder).split('').sort().join('');
-  			if (noDupes[newPrefix] === undefined) {
-				return searcher(newPrefix, remainderCopy);
-  			}
-  		});
-  	};
+      remainder.forEach((letter, index) => {
+        var remainderCopy = remainder.slice();
+        var popRemainder = remainderCopy.splice(index, 1);
+        var newPrefix = (prefix + popRemainder).split('').sort().join('');
+        if (noDupes[newPrefix] === undefined) {
+        return searcher(newPrefix, remainderCopy);
+        }
+      });
+    };
 
-  	while (letters.length > 1) {
-  		searcher(letters.shift(), letters);
-  	}
+    while (letters.length > 1) {
+      searcher(letters.shift(), letters);
+    }
 
-  	Object.keys(noDupes).forEach(key => {
-  		if (noDupes[key] !== null) {
-  			console.log(noDupes[key])
-  		}
-  	});
+    Object.keys(noDupes).forEach(key => {
+      if (noDupes[key] !== null) {
+        console.log(noDupes[key])
+      }
+    });
   }
 };
 
