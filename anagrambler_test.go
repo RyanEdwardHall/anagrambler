e0217e565056ff3d1d5f94b629d18258e5dabb5d
Dan McGinnis <dan@mcgid.ca>
2016-05-20 15:00:57 -0400

    Move test data into table, and abstract test methods
    This will allow us to get more specific about which data we're invoking
    with which method.

--- anagrambler_test.go
+++ anagrambler_test.go
@@ -6,26 +6,39 @@ import (
 	"github.com/RyanEdwardHall/anagrambler"
 )
 
-func TestKnownOutput(t *testing.T) {
+type dataItem struct {
+	dict string
+	input string
+	anagrams int
+}
+
+var testData = []dataItem {
+	{"go-dict.txt", "honorificabilitudinitatibus", 9083},
+	{"go-dict.txt", "Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon", 112436},
+}
+
+func testAnagramCount(t *testing.T, d dataItem) {
 	trie := anagrambler.NewNode()
 
-	anagrambler.LoadDict(trie, "go-dict.txt")
+	anagrambler.LoadDict(trie, d.dict)
 
-	searchWord := "honorificabilitudinitatibus"
+	searchWord := d.input
 
 	results := anagrambler.Search(trie, searchWord, "")
 
-	if len(results) != 9083 {
-		t.Error("Expected 9083 words, got ", len(results))
+	if len(results) == d.anagrams {
+		t.Logf("Success: found all %d expected anagrams for '%s'\n", d.anagrams, d.input)
+	} else {
+		t.Error("Expected", d.anagrams, "words, got ", len(results))
 	}
 }
 
-func BenchmarkAnagrambler(b *testing.B) {
+func benchmarkSearch(b *testing.B, d dataItem) {
 	trie := anagrambler.NewNode()
 
-	anagrambler.LoadDict(trie, "go-dict.txt")
+	anagrambler.LoadDict(trie, d.dict)
 
-	searchWord := "Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon"
+	searchWord := d.input
 
 	b.ResetTimer()
 
@@ -33,3 +46,9 @@ func BenchmarkAnagrambler(b *testing.B) {
 		anagrambler.Search(trie, searchWord, "")
 	}
 }
+
+func TestAnagramCountShort(t *testing.T) { testAnagramCount(t, testData[0]) }
+func TestAngaramCountLong(t *testing.T) { testAnagramCount(t, testData[1]) }
+
+func BenchmarkSearchShort(b *testing.B) { benchmarkSearch(b, testData[0]) }
+func BenchmarkSearchLong(b *testing.B) { benchmarkSearch(b, testData[1]) }
