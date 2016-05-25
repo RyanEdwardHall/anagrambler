6186d08c725f214fee379eeb6d84d8b35d42566c
Dan McGinnis <dan@mcgid.ca>
2016-05-25 16:29:00 -0400

    Run gofmt on _test file

--- anagrambler_test.go
+++ anagrambler_test.go
@@ -10,15 +10,15 @@ import (
 )
 
 type dataItem struct {
-	dict string
-	input string
-	filter string
+	dict     string
+	input    string
+	filter   string
 	anagrams int
 }
 
 // Test data and test fixtures
 var (
-	testData = []dataItem {
+	testData = []dataItem{
 		{"go-dict.txt", "honorificabilitudinitatibus", "", 9083},
 		{"go-dict.txt", "honorificabilitudinitatibus", "bus", 34},
 		{"go-dict.txt", "pneumonoultramicroscopicsilicovolcanoconiosis", "", 26035},
@@ -29,11 +29,9 @@ var (
 	testTrie = anagrambler.NewNode()
 )
 
-
 func init() {
 	anagrambler.LoadDict(testTrie, testData[0].dict)
 }
-
 
 func testAnagramCount(t *testing.T, d dataItem) {
 	results := anagrambler.Search(testTrie, d.input, d.filter)
@@ -44,7 +42,6 @@ func testAnagramCount(t *testing.T, d dataItem) {
 		t.Error("Expected", d.anagrams, "words, got ", len(results))
 	}
 }
-
 
 func benchmarkFillTrie(b *testing.B, dictPath string) {
 	data, err := ioutil.ReadFile(dictPath)
@@ -71,18 +68,21 @@ func benchmarkSearch(b *testing.B, d dataItem) {
 	}
 }
 
-func TestAnagramCountShort(t *testing.T) { testAnagramCount(t, testData[0]) }
-func TestAnagramCountShortFiltered(t *testing.T) { testAnagramCount(t, testData[1]) }
-func TestAnagramCountMedium(t *testing.T) { testAnagramCount(t, testData[2]) }
+// Tests for verifying correct output (i.e. correct number of anagrams for given input).
+func TestAnagramCountShort(t *testing.T)          { testAnagramCount(t, testData[0]) }
+func TestAnagramCountShortFiltered(t *testing.T)  { testAnagramCount(t, testData[1]) }
+func TestAnagramCountMedium(t *testing.T)         { testAnagramCount(t, testData[2]) }
 func TestAnagramCountMediumFiltered(t *testing.T) { testAnagramCount(t, testData[3]) }
-func TestAngaramCountLong(t *testing.T) { testAnagramCount(t, testData[4]) }
-func TestAngaramCountLongFilter(t *testing.T) { testAnagramCount(t, testData[5]) }
+func TestAngaramCountLong(t *testing.T)           { testAnagramCount(t, testData[4]) }
+func TestAngaramCountLongFilter(t *testing.T)     { testAnagramCount(t, testData[5]) }
 
+// Benchmarks for loading a dictionary file into a trie.
 func BenchmarkFillTrie(b *testing.B) { benchmarkFillTrie(b, testData[0].dict) }
 
-func BenchmarkSearchShort(b *testing.B) { benchmarkSearch(b, testData[0]) }
-func BenchmarkSearchShortFiltered(b *testing.B) { benchmarkSearch(b, testData[1]) }
-func BenchmarkSearchMedium(b *testing.B) { benchmarkSearch(b, testData[2]) }
+// Benchmarks for searching for anagrams.
+func BenchmarkSearchShort(b *testing.B)          { benchmarkSearch(b, testData[0]) }
+func BenchmarkSearchShortFiltered(b *testing.B)  { benchmarkSearch(b, testData[1]) }
+func BenchmarkSearchMedium(b *testing.B)         { benchmarkSearch(b, testData[2]) }
 func BenchmarkSearchMediumFiltered(b *testing.B) { benchmarkSearch(b, testData[3]) }
-func BenchmarkSearchLong(b *testing.B) { benchmarkSearch(b, testData[4]) }
-func BenchmarkSearchLongFiltered(b *testing.B) { benchmarkSearch(b, testData[5]) }
+func BenchmarkSearchLong(b *testing.B)           { benchmarkSearch(b, testData[4]) }
+func BenchmarkSearchLongFiltered(b *testing.B)   { benchmarkSearch(b, testData[5]) }
